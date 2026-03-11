ddae67cf91428354d6145fc078c62620f9abb8c4
Charles ASSOGBA <charlesclaudepascal@gmail.com>
2026-03-11 08:57:17 +0800

    fix: complete button and saveBtn function implementation

--- CRUD-localStorage/crud.js
+++ CRUD-localStorage/crud.js
@@ -1,30 +1,29 @@
 const input = document.querySelector(".input");
 const button = document.querySelector(".btn");
 
-
 //get input value by clicking the button +Todo
 button.addEventListener("click", () => {
   let inputValue = input.value.trim();
-  if (inputValue!=="") {
+  if (inputValue !== "") {
     add(inputValue);
   }
 });
 //function to add items
-const add = (inputValue) =>{
- //create the localStorage and push input data into
+const add = (inputValue) => {
+  //create the localStorage and push input data into
   let localData = JSON.parse(localStorage.getItem("localData")) || [];
   localData.push(inputValue.trim());
   //set the value of the localStorage (convert in JSON) to that localData I created😏
   localStorage.setItem("localData", JSON.stringify(localData));
   display();
-}
+};
 //function to display
-const display = (inputValue) =>{
-//Here i insert the localData element into the table
+const display = (inputValue) => {
+  //Here i insert the localData element into the table
   if (inputValue !== "") {
     let localData = JSON.parse(localStorage.getItem("localData")) || [];
     const tab = document.querySelector(".tab");
-    tab.innerHTML="";
+    tab.innerHTML = "";
     localData.forEach((element, index) => {
       let tr = document.createElement("tr");
       tr.innerHTML = `
@@ -43,26 +42,53 @@ const display = (inputValue) =>{
 
 //complete function
 const completeBtn = (button) => {
-     let localData = JSON.parse(localStorage.getItem("localData")) || [];
-     localData.push({text:button, completed: true});
-     localStorage.setItem("localData", JSON.stringify(localData));
-     console.log(`Item "${button}" added to completed list.`);
-     const todoItem = button.parentElement.parentElement;
-     const toogle = todoItem.classList.toggle("completed");
-     console.log(toogle?"is on":"is off");
-};
-
-//edit function
-const editBtn = (index) => {
-
+  /*
+  let localData = JSON.parse(localStorage.getItem("localData")) || [];
+  localData.push({ text: index, completed: true });
+  localStorage.setItem("localData", JSON.stringify(localData));
+  */
+  const todoItem = button.parentElement.parentElement;
+  const toogle = todoItem.classList.toggle("completed");
+  console.log(toogle ? "is on" : "is off");
 };
 
 //delete function
 const deleteBtn = (index) => {
+  //retrieve the data from the localStorage
   let localData = JSON.parse(localStorage.getItem("localData")) || [];
-   localData.splice(index, 1);
-   localStorage.setItem("localData", JSON.stringify(localData));
-   display();
+  localData.splice(index, 1);
+  //update the localStorage
+  localStorage.setItem("localData", JSON.stringify(localData));
+  display();
 };
+
+//edit function
+const editBtn = (index) => {
+  let localData = JSON.parse(localStorage.getItem("localData")) || [];
+    const tab = document.querySelector(".tab");
+    tab.innerHTML = "";
+    localData.forEach((element, index) => {
+      let tr = document.createElement("tr");
+      tr.innerHTML = `
+                <td>
+                <input type="text" value="${element}"/>
+                </td>
+                <td>
+                  <button class="btnComplete" onClick="completeBtn(this)">Complete</button>
+                  <button class="btnSave" onClick="saveBtn(${index})">Save</button>
+                  <button class="btnDelete" onClick="deleteBtn(${index})">Delete</button>
+                </td>  
+      `;
+      tab.appendChild(tr);
+      input.value = "";
+    });
+};
+
+//save function
+const saveBtn = (index)=>{
+console.log(value);
+}
+
+
 
 display();
